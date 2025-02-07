915eba0dfeac0dffad6b029bb6357cbdf86337ca
rootsystemos12423 <osornocrypto@protonmail.com>
2025-02-07 14:13:37 -0300

    Rabbit

--- app/Http/Controllers/WhiteRabbitController.php
+++ app/Http/Controllers/WhiteRabbitController.php
@@ -83,9 +83,7 @@ class WhiteRabbitController extends Controller
         $domain = $request->getHost();
         $safePage = "https://{$domain}/{$id}/safe";
 
-      // $ip = $request->header('CF-Connecting-IP') ?? $request->ip();
-
-        $ip = '177.75.75.230';
+       $ip = $request->header('CF-Connecting-IP') ?? $request->ip();
 
         $apiUrl = "http://ip-api.com/json/{$ip}?fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,reverse,mobile,proxy,hosting,query";
 
