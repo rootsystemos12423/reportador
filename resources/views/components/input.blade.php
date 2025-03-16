dd304d54454f1196e76d64f4a361b829eedb7cd3
rootsystemos12423 <osornocrypto@protonmail.com>
2025-03-16 12:13:51 -0300

    Changes

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-purple-600 border-2 bg-zinc-900 text-white focus:border-purple-500 focus:ring-purple-500 rounded-md shadow-sm']) !!}>
